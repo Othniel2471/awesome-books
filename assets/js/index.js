70502f50a6159803d9b136e230819a876a723fed
othniel2471 <othnielomaidu@gmail.com>
2023-05-29 09:56:09 +0100

    added the html structure for the project and linked the js file to the index.html file

---
+++ assets/js/index.js
@@ -0,0 +1,5 @@
+const form = document.querySelector('form');
+const bookTitle = document.querySelector('.title');
+const bookAuthor = document.querySelector('.author');
+const addBook = document.querySelector('.add');
+const bookContainer = document.querySelector('.books-container');
