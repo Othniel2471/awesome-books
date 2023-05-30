7667ff6d0c35fb4302b16c52582641ec32dd99db
MARVIN <o.silverygold@gmail.com>
2023-05-30 13:19:35 +0300

    Update the JS to use Classes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,76 +1,97 @@
-const form = document.querySelector('form');
-const bookTitle = document.querySelector('.title');
-const bookAuthor = document.querySelector('.author');
+class Books {
+  static books = [];
 
-// book array
+  static form;
 
-const addBooks = () => {
-  const bookDetails = {
-    title: bookTitle.value,
-    author: bookAuthor.value,
-  };
+  static bookTitle;
 
-  let books = [];
+  static bookAuthor;
 
-  if (localStorage.getItem('books')) {
-    books = JSON.parse(localStorage.getItem('books'));
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
   }
-  books.push(bookDetails);
-  localStorage.setItem('books', JSON.stringify(books));
-};
 
-const clearField = () => {
-  bookTitle.value = '';
-  bookAuthor.value = '';
-};
+  static initialize() {
+    Books.form = document.querySelector('form');
+    Books.bookTitle = document.querySelector('.title');
+    Books.bookAuthor = document.querySelector('.author');
 
-const displayBooks = (book) => {
-  const books = JSON.parse(localStorage.getItem('books'));
-  const bookContainer = document.querySelector('.books-container');
-  const displaybook = book.map((item) => `
-        <div id="book" data-title="${item.title}">
-        <p>${item.title}</p>
-        <p>${item.author}</p>
-        <button class="remove">Remove</button>
-        <hr>
-        </div>
-        `).join('');
+    const inv = new Books();
+    inv.submitForm();
 
-  // display books
-  bookContainer.innerHTML = displaybook;
-
-  bookContainer.addEventListener('click', (event) => {
-    if (event.target.classList.contains('remove')) {
-      // Find the parent book element and remove it
-      const bookElement = event.target.closest('#book');
-      const bookTitle = bookElement.dataset.title;
-
-      bookElement.remove();
-
-      // Remove the book from the 'books' array
-      const bookIndex = books.findIndex((book) => book.title === bookTitle);
-      if (bookIndex !== -1) {
-        books.splice(bookIndex, 1);
+    (function onReload() {
+      if (localStorage.getItem('books')) {
+        const allBooks = JSON.parse(localStorage.getItem('books'));
+        inv.displayBooks(allBooks);
       }
-      localStorage.setItem('books', JSON.stringify(books));
+    }());
+  }
+
+  addBooks() {
+    const title = Books.bookTitle.value;
+    const author = Books.bookAuthor.value;
+    const bookDetails = new Books(title, author);
+
+    if (localStorage.getItem('books')) {
+      this.books = JSON.parse(localStorage.getItem('books'));
     }
-  });
-};
+    this.books.push(bookDetails);
+    localStorage.setItem('books', JSON.stringify(this.books));
+  }
 
-const displayItem = () => {
-  addBooks();
-  const updatedBooks = localStorage.getItem('books');
-  const allBooks = JSON.parse(updatedBooks);
-  displayBooks(allBooks);
-  clearField();
-};
+  clearField() {
+    this.constructor.bookTitle.value = '';
+    this.constructor.bookAuthor.value = '';
+  }
 
-(function onReload() {
-  const allBooks = JSON.parse(localStorage.getItem('books'));
-  displayBooks(allBooks);
-}());
+  // eslint-disable-next-line class-methods-use-this
+  displayBooks(book) {
+    const books = JSON.parse(localStorage.getItem('books'));
+    const bookContainer = document.querySelector('.books-container');
+    const displaybook = book.map((item) => `
+            <div id="book" data-title="${item.title}">
+              <p>${item.title}</p>
+              <p>${item.author}</p>
+              <button class="remove">Remove</button>
+              <hr>
+              </div>
+              `).join('');
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  displayItem();
-});
+    // display books
+    bookContainer.innerHTML = displaybook;
+
+    bookContainer.addEventListener('click', (event) => {
+      if (event.target.classList.contains('remove')) {
+        // Find the parent book element and remove it
+        const bookElement = event.target.closest('#book');
+        const bookTitle = bookElement.dataset.title;
+
+        bookElement.remove();
+        // Remove the book from the 'books' array
+        const bookIndex = books.findIndex((book) => book.title === bookTitle);
+        if (bookIndex !== -1) {
+          books.splice(bookIndex, 1);
+        }
+        localStorage.setItem('books', JSON.stringify(books));
+      }
+    });
+  }
+
+  displayItem() {
+    this.addBooks();
+    const updatedBooks = localStorage.getItem('books');
+    const allBooks = JSON.parse(updatedBooks);
+    this.displayBooks(allBooks);
+    this.clearField();
+  }
+
+  submitForm() {
+    Books.form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      this.displayItem();
+    });
+  }
+}
+
+Books.initialize();
